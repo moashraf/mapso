cbb94948a5a24194116a45d8775094ee96b82f9b
figo <figo@figo.com>
2018-01-21 10:48:08 +0200

    nooooot

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,8 +12,26 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
    
-     //$this->call(ServicesSeeder::class); 
-  
+
+
+   /*  $this->call(AboutusSeeder::class); 
+     $this->call(Boats_catSeeder::class); 
+     $this->call(Boatsseeder::class); 
+     $this->call(ContactusSeeder::class); 
+     $this->call(LandingpageSeeder::class); 
+     $this->call(Newsseeder::class); 
+     $this->call(Projectsseeder::class); 
+     $this->call(PropulsionSeeder::class); 
+     $this->call(ServicesSeeder::class); 
+     $this->call(SingleboatGallerySeeder::class); 
+     $this->call(SitesettingsSeeder::class); 
+     $this->call(Slider11seeder::class); 
+     $this->call(SubscribeSeeder::class); 
+     $this->call(TestimonialSeeder::class);
+          $this->call(UsersSeeder::class); 
+ */
+          
+ 
      
  
     }
---
+++ database/seeds/UsersSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+for ($i=0; $i <1 ; $i++) { 
+$add=new User;
+ 
+$add->name="figoashraf";
+$add->email="figoashraf@gmail.com";
+$add->password= bcrypt("figoashraf");
+ 
+
+$add->save();
+}
+    }
+}
