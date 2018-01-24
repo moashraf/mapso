ebea4006b309fbcf7c9379ec5a44ba4694a629ef
figo <figo@figo.figo>
2018-01-24 10:19:45 -0800

    nooooot

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,9 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+     Route::group(['middleware'=>'auth:api' ,'prefix'=>'mop'], function(){
+ 
+   Route::resource('Projects','apiController');
+
+
+ });
